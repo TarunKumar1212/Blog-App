6aaaf9bbdc88636d98dc1fcb96d51b449754ba84
Tarun Kumar <14itarunkumar@gmail.com>
2024-04-26 23:21:18 +0530

    react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Container from "./container/container";
+import logo from "./logo"
+import logoutBtn from "./Header/logoutBtn"
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    logo,
+    logoutBtn
 }
