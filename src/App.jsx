bd3e2bc32e018d5df69dd6bfa13807314406596a
Tarun Kumar <14itarunkumar@gmail.com>
2024-04-26 20:27:28 +0530

    intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component, useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import {Header,Footer} from './components/index'
 
 function App() {
-  
 
-  return (
-    <>
-      <h1>Mega Blog App</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap conten-between bg-gray-400'>
+      <div className="w-full block">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>  
+      </div> 
+    </div>
+  ) :<div>
+    Data is Loading
+  </div>
 }
 
 export default App
