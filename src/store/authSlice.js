205b9f0943a342fc86d98c5d682b5fcd74e2999d
Tarun Kumar <14itarunkumar@gmail.com>
2024-04-26 19:31:41 +0530

    configure redux toolkit

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    staus : false,
+    userData : null
+}
+
+const authSlice = createSlice({
+    name : "auth",
+    initialState,
+    reducers : {
+        login : (state,action)=> {
+            state.staus = true;
+            state.userData = action.payload.userData;
+        },
+        logout : (state) =>{
+            state.staus = false;
+            state.userData = null;
+        }
+    }  
+})
+
+
+
+export default authSlice.reducer ;
+
+export const {login,logout} = authSlice.actions ;
