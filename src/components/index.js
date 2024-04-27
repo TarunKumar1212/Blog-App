7ea479393bc3fcae03c13ffc2b1aa429a14b80ab
Tarun Kumar <14itarunkumar@gmail.com>
2024-04-27 22:50:10 +0530

    PostForm

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import logo from "./logo"
 import logoutBtn from "./Header/logoutBtn"
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Container,
     logo,
-    logoutBtn
+    logoutBtn,
+    RTE
 }
