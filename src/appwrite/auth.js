24d50cb1a14f5dce121f921d9798dd529883f76d
Tarun Kumar <14itarunkumar@gmail.com>
2024-09-02 19:18:42 +0530

    updated changes

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,5 @@
-import conf from "../conf/conf";
+import conf from '../conf/conf.js';
+
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService{
@@ -14,7 +15,7 @@ export class AuthService{
 
     async createAccount({email,password,name}){
         try {
-            const userAccount = await this.account.create(ID.unique,email,password,name);
+            const userAccount = await this.account.create(ID.unique(),email,password,name);
             if (userAccount) {
                 //call another method
                 return this.login({email,password});
